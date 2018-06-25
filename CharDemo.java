c858317e4829986b40965619ac33824cb09fde78
VeryAble <VaryAble@protonmail.ch>
2018-06-25 14:09:09 -0700

    Create CharDemo.java

---
+++ CharDemo.java
@@ -0,0 +1,16 @@
+/* CharDemo.java Demonstrates some features of the char data type
+*  Author: VaryAble
+*  Date : 5/26/2018
+*/
+
+public class CharDemo
+{
+    public static void main(String[] args)
+    {
+        char initial = 'A';
+
+        System.out.println(initial);
+        System.out.println("\t\"abc\\def\bghi\n\njkl");
+    }
+    // Thanks for looking
+}
