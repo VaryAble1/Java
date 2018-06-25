320d0338729ed16387d75c990571692a1a353e0c
VeryAble <VaryAble@protonmail.ch>
2018-06-25 14:13:02 -0700

    Create FirstDialog.java

---
+++ FirstDialog.java
@@ -0,0 +1,13 @@
+/* FirstDialog.java Display a dialog GUI message
+   Author: VaryAble
+   Date: 5/23/2018
+ */
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "First Java Dialog");
+    }
+    // Thanks for looking
+}
