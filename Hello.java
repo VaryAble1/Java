db96eb1defa57943029ba62f82749a0401a3725b
VeryAble <VaryAble@protonmail.ch>
2018-06-25 14:11:03 -0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/* Print message to console
+   Author: VaryAble
+   Date: 5/22/2018
+ */
+
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+    // Thanks for looking
+}
