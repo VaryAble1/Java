fd37627b065698fa26972922fa4980ce7eb5618a
VeryAble <VaryAble@protonmail.ch>
2018-06-25 14:12:06 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+/* Main.java Print message to console
+   Author: VaryAble
+   Date: 5/22/2018
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("First Java Application");
+    }
+    // Thanks for looking
+}
