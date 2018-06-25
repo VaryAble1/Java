ca8c343f22e32d879b69c4f9448a6a0591fcaa50
VeryAble <VaryAble@protonmail.ch>
2018-06-25 14:10:02 -0700

    Create DataDemo.java

---
+++ DataDemo.java
@@ -0,0 +1,21 @@
+/* DataDemo.java work with a variable and a constant
+*  Author: VaryAble
+*  Date: 5/24/2018
+*/
+
+public class DataDemo
+{
+    public static void main(String[] args)
+    {
+        // Building Blocks
+        int aWholeNumber = 315;
+        final int STATES_IN_US = 50;
+
+        // Output
+        System.out.print("The number is ");
+        System.out.println(aWholeNumber);
+        System.out.println("The number of states is " +
+        STATES_IN_US);
+    }
+    // Thanks for looking
+}
