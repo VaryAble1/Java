24e296c193ce989d53a4c3379c588df541ced79e
VaryAble <VaryAble@protonmail.ch>
2018-06-26 13:02:23 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,52 @@
+/*
+* Author: VaryAble
+* Date: 6/26/2018
+*/
+public class Employee
+{
+    private int empNum;
+    private String empLastName;
+    private String empFirstName;
+    private double empSalary;
+
+    public int etEmpNum()
+    {
+        return empNum;
+    }
+
+    public void setEmpNum(int emp)
+    {
+        empNum = emp;
+    }
+
+    public String getEmpLastName()
+    {
+        return empLastName;
+    }
+
+    public void setEmpLastName(String name)
+    {
+        empLastName = name;
+    }
+
+    public String getEmpFirstName()
+    {
+        return empFirstName;
+    }
+
+    public void setEmpFirstName(String name)
+    {
+        empFirstName = name;
+    }
+
+    public double getEmpSalary()
+    {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double sal)
+    {
+        empSalary = sal;
+    }
+// Thanks for looking
+}
