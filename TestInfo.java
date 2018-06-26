13a17db813d5325fc3576d569f1c2b78ebd0904f
VaryAble <VaryAble@protonmail.ch>
2018-06-26 12:10:30 -0700

    Create TestInfo.java

---
+++ TestInfo.java
@@ -0,0 +1,13 @@
+/* Call displayInfo() method of ParadiseInfo.java from a method within another class
+* Author: VaryAble
+* Date: 6/26/2018
+*/
+public class TestInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Calling method from another class:\n");
+        ParadiseInfo.displayInfo(); // From ParadiseInfo.java class
+    }
+// Thanks for looking
+}
