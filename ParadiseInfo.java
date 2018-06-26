622136476dc9c0a5ca77c3edb000d7f944e20053
VaryAble <VaryAble@protonmail.ch>
2018-06-26 12:04:42 -0700

    Create ParadiseInfo.java

---
+++ ParadiseInfo.java
@@ -0,0 +1,18 @@
+/* Display two messages
+* Author: VaryAble
+* Date: 6/26/2018
+*/
+public class ParadiseInfo
+{
+    public static void main(String[] args)
+    {
+        displayInfo();
+    }
+
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+// Thanks for looking
+}
