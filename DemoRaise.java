c6716e8dbac7c272e060e7536b3d88ac16e19a90
VaryAble <VaryAble@protonmail.ch>
2018-06-26 12:23:33 -0700

    Create DemoRaise.java

---
+++ DemoRaise.java
@@ -0,0 +1,26 @@
+/*
+* Author: VaryAble
+* Date: 6/26/2018
+*/
+public class DemoRaise
+{
+    public static void main(String[] args)
+    {
+        double salary = 200.00;
+        double startingWage = 800.00;
+        System.out.println("Demonstrating some raises");
+        predictRaise(400.00);
+        predictRaise(salary);
+        predictRaise(startingWage);
+    }
+
+    public static void predictRaise(double salary)
+    {
+        double newSalary;
+        final double RAISE_RATE = 1.10;
+        newSalary = salary * RAISE_RATE;
+        System.out.println("Current salary: " +
+        salary + "  After raise: " + newSalary);
+    }
+// Thanks for looking
+}
